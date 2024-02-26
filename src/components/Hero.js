79ea6c6b066d1ad186fc88b94677d4753f84cfb5
Gayathri Thejus <gayathrithejus@gmail.com>
2024-02-26 20:05:12 +0530

    hero updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,17 @@
 import React from 'react'
+
 import '../components/Hero.css'
 const Hero = () => {
-    return (
-       <div className='max-w-[1200px] flex flex-col mx-auto justify-center items-center'>
-         <svg viewBox='0 0 1350 600' className=" mx-auto text-center ml-[80px] ">
-  <text x='0' y="50%" fill="black" className='text-[120px] ' >
-    I speak.
-  </text>
-  <text x='0' y="70%" fill="black" className='text-[120px] '>
-    I code.
-  </text>
-  <text x='0' y="90%" fill="black" className='text-[120px]  '>
-    I design.
-  </text>
-</svg>  
-<div className=''><h6 className=' font-light text-lg mx-auto'>By Gayathri Thejus</h6></div>
-       </div>
-      )
-    }
-    
-    export default Hero
-    
-    
+  return (
+    <div className='text-black' >
+        <div className='max-w-[800px] flex flex-col w-full text-center mt-[126px] h-screen mx-auto'>
+
+            <h1 className='md:text-7xl sm:text-6xl text-5xl font-bold  md:py-6 bg-gradient-to-r from-cyan-500 to-lime-200 bg-clip-text text-transparent'>I Speak.<br/>I code.<br/>I design.</h1>
+            <p className='comforter md:2xl text-2xl font-light text-gray-500 pt-5'>Gayathri Thejus</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Hero
